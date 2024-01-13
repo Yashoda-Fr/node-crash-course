598a28a46c0a281dd11e9e3b6475ca9d3ed7308f
yashoda-fr <fyashodha@gmail.com>
2024-01-13 13:24:54 +0530

    Add MongoDB integration and create blog routes

--- app.js
+++ app.js
@@ -1,59 +1,115 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
-//connect to mongodb
-const dbURI ='mongodb+srv://netninjayasho:test12345@nodetuts.ogxy4gy.mongodb.net/?retryWrites=true&w=majority'
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninjayasho:test12345@nodetuts.ogxy4gy.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
-
-// listen for requests
-app.listen(3000);
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
 app.use(morgan('dev'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
+// mongoose & mongo tests
+app.get('/addBlog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  })
+
+  blog.save()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5ea99b49b8531f40c0fde689')
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
+// blog routes
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {  
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+  .then(result => 
+    res.render('details', { blog: result, title: 'Blog Details' })
+  )
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
+
 
 // 404 page
 app.use((req, res) => {
